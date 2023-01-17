9bf938f0ce1aeab39f832bdaff7110caacdba488
= <=>
2023-01-17 10:18:17 +0600

    1 lecture code

--- week1/code_example/main.py
+++ week1/code_example/main.py
@@ -1,1 +1,88 @@
-print("hello")
+"""
+Python Intro
+Python User Input
+Python Get Started
+Python Syntax
+Python Comments
+Python Variables
+Python Data Types
+Python Numbers
+Python Casting
+Python Strings
+Python String Formatting
+Python Booleans
+Python Operators
+Python If...Else
+Git
+"""
+# int
+a = 10
+
+# float
+b = 13.6
+
+# bool
+check = True
+
+# str
+text = 'Hello'
+
+# list
+list1 = []
+
+# tuple
+tupl2 = ()
+
+# set
+set1 = set()
+
+# dict
+dict1 = {}
+
+# print(type(dict1))
+
+"""
+kwjekjekjwje
+jwhejhjwhe
+"""
+
+a = 10
+name = "John"
+age = 20
+city = "New York"
+# formatting
+# print("My name is {0}, age is {1}, city is {2}".format(name, age, city))
+number = 10
+# print("yes" if number >= 0 else 'no')
+
+# a = int(input("Insert a: "))
+# print(a)
+
+# name = "Nina"
+# name2 = "Assel"
+# print(ord("N"))
+# print(ord("A"))
+
+# name = "Nina".upper()
+# name2 = "nina".upper()
+# print(name2 == name)
+
+
+# text2 = "za"
+# print(text2 in text)
+# print(text * 8)
+# print(len(text))
+# for i in text:
+#     print(i, end="-")
+# aziza
+# str methods
+# print(text == text[::-1])
+
+text = "my namea is Joahn"
+text_split = text.split("a")
+print(text_split)
+# range () 1 option 0 о конца
+# range () 2 option 2, 6
+# range () 3 option 0 о конца
+# for i in range(2, 10, 2):
+#     print(i)
